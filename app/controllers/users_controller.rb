97f7ed424e5f141843e342ecfdbf08fbb5f9fa1c
nomura0106 <oca2402pg0006@edu.oca.ac.jp>
2025-06-20 07:09:08 +0000

    1

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+   
+  
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user_show = User.find(params[:id])
+  end
+
+  def edit
+  end
+  def create
+     user = User.new(user_params)
+
+     if user.save
+       redirect_to root_path
+     else
+       render :new
+     end
+      end
+
+      private
+
+     def user_params
+       params.require(:user).permit(:first_name, :family_name)
+     end
+end
